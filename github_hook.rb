000b03d647fc53612f1fef23f182c4358d9b17d1
Dave Kaplan <workalicious@gmail.com>
2013-10-20 16:38:05 -0500

    working on nil for github time parse

--- github_hook.rb
+++ github_hook.rb
@@ -5,7 +5,6 @@ class GithubHook < Sinatra::Base
   def self.parse_git
     # Parse hash and date from the git log command.
     sha1, date ||= `git log HEAD~1..HEAD --pretty=format:%h^%ci`.strip.split('^')
-
     # set :commit_hash, sha1
     # set :commit_date, Time.parse(date)
 
